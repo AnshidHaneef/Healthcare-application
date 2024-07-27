8ba5d33e576ad9222a16c9ad5f7c6b8ee04b47b5
anshid <anshidhaneefa710@gmail.com>
2024-07-27 22:05:00 +0530

    user signIn ui setup

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Login =()=>{
-    return(
-        <>
-            
+export const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-        </>
-    )
+const handleSubmit=(event)=>{
+  const [form,setForm] = useState({
+
+  }, )
 }
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault() 
+  // };
+
+  return (
+    <div
+      className="flex justify-center items-center bg-fixed bg-center bg-cover h-screen "
+      style={{
+        backgroundImage:
+          "url(https://practicesuite.com/wp-content/uploads/2023/03/virtual-medical-clinic.webp)",
+      }}
+    >
+      <div className="box-content h-80  p-4 border-4 bg-teal-600 opacity-90 flex justify-start items-center flex-col rounded-md border-none  ">
+        <h3 className="text-white text-2xl font-semibold items-center flex justify-center mb-4 mt-10">
+          Sign In
+        </h3>
+        <form onSubmit={handleSubmit} >
+          <div className="p-2">
+            <input 
+              onChange={(event) => setEmail(event.target.value)}
+              type="email"
+              value={email}
+              placeholder="Enter Your email"
+              className=" px-5 text-sm  w-72 h-8 rounded-sm border-none "
+            />
+          </div>
+
+          <div className="p-2">
+            <input
+              type="password"
+              value={password}
+              onChange={(event)=>setPassword(event.target.value)}
+              placeholder="Enter your Password"
+              className=" px-5 w-72 text-sm h-8 rounded-sm border-none"
+            />
+          </div>
+          <button
+            type="submit"
+            className=" m-4 px-5 bg-blue-700 text-white w-72 h-8 rounded-md "
+          >
+            Submit
+          </button>
+        </form>
+
+        <p className="text-sm">
+          Dont have an accoount?
+          <a className="text-slate-300 font-thin underline" href="">
+            Register
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -3,24 +3,51 @@ import React from "react";
 export const Register = () => {
   return (
     <div
-      className="flex justify-center items-center bg-fixed bg-center bg-cover h-screen opacity-90"
+      className="flex justify-center items-center bg-fixed bg-center bg-cover h-screen "
       style={{
         backgroundImage:
           "url(https://practicesuite.com/wp-content/uploads/2023/03/virtual-medical-clinic.webp)",
       }}
     >
-      <div className="box-content h-96 w-96 p-4 border-4 bg-teal-600 opacity-90 ">
-        <h3 className="text-white text-2xl font-semibold items-center flex justify-center">
+      <div className="box-content h-96  p-4 border-4 bg-teal-600 opacity-90 flex justify-center items-center flex-col rounded-md border-none  ">
+        <h3 className="text-white text-2xl font-semibold items-center flex justify-center  ">
           Sign Up
         </h3>
-        <input
+        <h6 className="text-white  mb-2 font-light tracking-wider text-sm">It's quick and easy</h6>
+
+      <div className="p-2">
+      <input
           type="text"
           placeholder="Your Name Here"
-          className=" m-4 px-10 flex  items-center justify-center "
+          className=" px-5 flex text-sm w-72 h-8 rounded-sm border-none "
         />
-        <input type="number" placeholder="Enter your phone Number" className="m-4 px-10" />
-        <input type="email"  placeholder="Enter Your email" className="m-4 px-10" />
-        <button className=" m-4 px-10 ml-20 "> Submit</button>
+      </div>
+
+       <div className="p-2">
+       <input
+          type="number"
+          placeholder="Enter your phone Number"
+          className=" px-5 w-72 text-sm h-8 rounded-sm border-none"
+        />
+       </div>
+
+      <div className="p-2">
+              <input
+                type="email"
+                placeholder="Enter Your email"
+                className=" px-5 text-sm  w-72 h-8 rounded-sm border-none "
+              />
+      </div>
+
+      <div className="p-2">
+              <input
+                type="password"
+                placeholder="Choose a Password"
+                className=" px-5 w-72 text-sm h-8 rounded-sm border-none"
+              />
+      </div>
+        <button className=" m-4 px-5 bg-blue-700 text-white w-72 h-8 rounded-md "> Submit</button>
+        <p className="text-sm">Already a member? <a className="text-slate-300 font-thin underline" href="">Login Here</a> </p>
       </div>
     </div>
   );
